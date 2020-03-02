93b4aeca4f92463e7f41c361f620adaf51e30c81
James_Git <franciscusjames01@gmail.com>
2020-03-02 15:20:09 -0300

    ajuste apresentação emails na tela

--- src/modules/dashboard/containers/dashboard/dashboard.component.ts
+++ src/modules/dashboard/containers/dashboard/dashboard.component.ts
@@ -12,7 +12,8 @@ import { DashboardService } from '../../services/dashboard.service';
 export class DashboardComponent implements OnInit {
     public emailsNaoLidosCount = [];
     public emailsFiltrados = [];
-    public displayTela = [];
+    public displayTela: any;
+    // public displayTela10: (() => Promise<any>) | undefined;
     public historico = [];
 
     // constructor(private service: DashboardService, private loader: LoaderService) {}
@@ -22,6 +23,7 @@ export class DashboardComponent implements OnInit {
         // this.loader.show();
         await this.fillEmailsFiltrados();
         await this.fillEmailsCount();
+        // this.displayTela10 = await this.service.getEmailsNaoLidos10dias;
         // this.loader.hide();
     }
 
@@ -47,36 +49,51 @@ export class DashboardComponent implements OnInit {
         // this.loader.show();
         console.log('10 dias OK');
         this.displayTela = await this.service.getEmailsNaoLidos10dias();
+        await this.formatData();
         // this.loader.hide();
     }
 
     async show7dias() {
         console.log('7 dias OK');
         this.displayTela = await this.service.getEmailsNaoLidos7dias();
+        await this.formatData();
     }
 
     async show5dias() {
         console.log('5 dias OK');
         this.displayTela = await this.service.getEmailsNaoLidos5dias();
+        await this.formatData();
     }
 
     async show3dias() {
         console.log('3 dias OK');
         this.displayTela = await this.service.getEmailsNaoLidos3dias();
+        await this.formatData();
     }
 
     async show2dias() {
         console.log('2 dias OK');
         this.displayTela = await this.service.getEmailsNaoLidos2dias();
+        await this.formatData();
     }
 
     async show1dia() {
         console.log('24hs OK');
         this.displayTela = await this.service.getEmailsNaoLidos1dia();
+        await this.formatData();
     }
 
     async getHistorico(assunto: string) {
         console.log('getHistorico OK');
         this.historico = await this.service.postHistoricoEmail(assunto);
+    }
+
+    async formatData() {
+        this.displayTela.map((email: { dataChegadaOuEnvio: { toString: () => string } }) => {
+            email.dataChegadaOuEnvio = email.dataChegadaOuEnvio
+                .toString()
+                .replace('T', '  ')
+                .replace('.000Z', '');
+        });
     }
 }
